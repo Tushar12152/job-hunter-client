d81566128ba7093c5b9d914166c3d1ee6000c95d
Tushar12152 <tusharimran470@gmail.com>
2023-11-05 07:14:45 -0700

    contact us section added

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,62 @@
+
+const Contact = () => {
+    return (
+        <div>
+              <form>
+          
+
+
+          <div className="flex w-full   gap-2">
+             
+        
+
+
+              <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">First Name</span>
+                </label>
+                <input name="salary" type="text" placeholder="First Name" className="input input-bordered input-primary w-full" required />
+              </div>
+
+              <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">Last Name</span>
+                </label>
+                <input name="salary" type="text" placeholder="Last Name" className="input input-bordered input-primary w-full" required />
+              </div>
+
+
+
+
+
+          </div>
+
+
+          <div className="flex w-full   gap-2">
+             
+          <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input name="description" type="text" placeholder="Email" className="input input-bordered input-primary w-full " required />
+              </div>
+
+              <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">Phone</span>
+                </label>
+                <input name="description" type="text" placeholder="Phone" className="input input-bordered input-primary w-full " required />
+              </div>
+
+          </div>
+
+         <textarea placeholder="Write Your Text.................." className="border-2 input-primary w-full mt-6" name="" id="" cols="30" rows="10"></textarea>
+         
+
+             <input className="btn w-full mt-5 bg-gradient-to-r from-red-500 to-blue-500" type="submit" value="Send" />
+        </form>
+        </div>
+    );
+};
+
+export default Contact;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,6 @@
 import AboutUs from "../Components/AboutUs";
 import Banner from "../Components/Banner";
+import Contact from "../Components/Contact";
 
 import JobCategory from "../Components/JobCategory";
 
@@ -12,6 +13,11 @@ const Home = () => {
             <h1 className='bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent text-2xl lg:text-5xl font-semibold lg:font-bold text-center pt-20 pb-10'>About Us</h1>
 
             <AboutUs></AboutUs>
+
+
+            <h1 className='bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent text-2xl lg:text-5xl font-semibold lg:font-bold text-center pt-20 pb-10'>Contact Us</h1>
+
+            <Contact></Contact>
 
         
         </div>
