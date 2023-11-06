26b27e00334debbf490803465b088169d497a21a
Tushar12152 <tusharimran470@gmail.com>
2023-11-06 07:18:14 -0700

    searchbar implement in all jobs. if user  write job title in searchbar he will see only thats titles job

--- src/Components/AllJobs.jsx
+++ src/Components/AllJobs.jsx
@@ -1,4 +1,7 @@
+
+
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import Job from "./job";
 
 const AllJobs = () => {
@@ -8,16 +11,37 @@ const AllJobs = () => {
           fetch('http://localhost:5002/jobs').then(
             (res) => res.json(),
           ),
-      })
+    });
 
-// console.log(data);
+    const [titleSearch, setTitleSearch] = useState('');
+
+    
+
+    const filteredJobs = data && data.filter(job => job.title.toLowerCase().includes(titleSearch.toLowerCase()));
+   
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[95%] mx-auto gap-6">
-            {
-               data&& data.map(job=><Job key={job._id} job={job}></Job>)
-            }
-        </div>
+       <div>
+           <form >
+               <div className="flex items-center justify-center gap-2 py-5">
+                   <div className="form-control w-[50%] flex items-center justify-center">
+                       <input 
+                           name="search" 
+                           type="search" 
+                           placeholder="Search by Job Title..." 
+                           className="input input-bordered input-primary w-full" 
+                           required
+                           value={titleSearch}
+                           onChange={(e) => setTitleSearch(e.target.value)}
+                       />
+                   </div>
+                   <input className="btn bg-gradient-to-r from-red-500 to-blue-500" type="submit" value="Search" />
+               </div>
+           </form>
+           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[95%] mx-auto gap-6">
+               {filteredJobs ? filteredJobs.map(job => <Job key={job._id} job={job}></Job>) : (data && data.map(job => <Job key={job._id} job={job}></Job>))}
+           </div>
+       </div>
     );
 };
 
