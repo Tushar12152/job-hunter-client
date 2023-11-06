fe81c44d1201752bfc0f6fcc2495b0d6c8bfb20a
Tushar12152 <tusharimran470@gmail.com>
2023-11-06 06:29:36 -0700

    in applied page implement select and option filter  by job category is successfull

--- src/page/AppliedJobs.jsx
+++ src/page/AppliedJobs.jsx
@@ -4,8 +4,19 @@ import { useEffect, useState, } from "react";
 import TotalApply from "../Components/TotalApply";
 
 const AppliedJobs = () => {
+
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleSelectChange = (e) => {
+      setSelectedOption(e.target.value);
+    };
+  
+
+
   const { user } = useAuth();
   const [usersApply,setUsersApply]=useState([])
+//   const [filterCategoryApply,setFilterCategoryapply]=useState([])
   const usersMail = user?.email;
 //   console.log(usersMail);
 
@@ -28,14 +39,45 @@ const AppliedJobs = () => {
     }
 
     if (applied) {
-      const filterAplly = applied.filter(job => job.email === usersMail);
-    //   console.log(filterAplly);
-    setUsersApply(filterAplly)
+        let filteredApply = applied.filter(job => job.email === usersMail);
+
+        if (selectedOption && selectedOption !== "All") {
+            filteredApply = filteredApply.filter(job => job.category === selectedOption);
+        }
+
+        setUsersApply(filteredApply);
     }
-  }, [applied, error, usersMail]);
+  }, [applied, error, usersMail,selectedOption]);
+
+  console.log(selectedOption);
 
   return (
-    <div className="w-[95%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+   <div className="w-[95%] mx-auto">
+
+<div className="mb-10">
+
+<select
+    id="mySelect"
+    className="input input-bordered input-primary w-[20%]"
+    value={selectedOption}
+    onChange={handleSelectChange}
+    required
+>
+    <option value="All">All</option>
+    <option value="On Site Job">On Site Job</option>
+    <option value="Remote Job">Remote Job</option>
+    <option value="Hybrid">Hybrid</option>
+    <option value="Part Time">Part Time</option>
+</select>
+</div>
+
+
+     <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+
+
+
+
       {
          usersApply.map(apply=><TotalApply
             key={apply._id}
@@ -43,6 +85,7 @@ const AppliedJobs = () => {
             ></TotalApply>)
       }
     </div>
+   </div>
   );
 };
 
